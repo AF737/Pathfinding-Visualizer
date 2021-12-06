62c739d629ed0c537ea05f9e1838fbb0f5a60ccc
AF737 <83659747+AF737@users.noreply.github.com>
2021-12-06 15:53:57 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 'use strict';
 
 import Node from './node.js';
+import Board from './board.js';
+import {dijkstra} from './algorithms/dijkstra.js';
 
 document.addEventListener('DOMContentLoaded', function() {
+    var dijkstraButton = document.getElementById('dijkstra');
+    var gridBoard = new Board();
+    var START_COL, START_ROW, FINISH_COL, FINISH_ROW;
+    const ANIMATION_SPEED = 10;
+
+    /* TODO:
+       - Clean up code
+       - Better animation when a node is visited
+       - Animate shortest path when node is found
+       - Get shortest path by backtracking from finish to start */
+
+    dijkstraButton.addEventListener('click', function() {
+        let startNode = gridBoard.nodesMatrix[START_ROW][START_COL];
+        let finishNode = gridBoard.nodesMatrix[FINISH_ROW][FINISH_COL];
+
+        const visitedNodes = dijkstra(gridBoard, startNode, finishNode);
+        //console.log(visitedNodes);
+        animateDijkstra(visitedNodes);
+    });
+
+    function animateDijkstra(visitedNodes) {
+        for (let i = 0; i < visitedNodes.length; i++) {
+            setTimeout(function() {
+                const currentNode = visitedNodes[i];
+                console.log(currentNode);
+                document.getElementById(`node-${currentNode.row}-${currentNode.column}`)
+                    .className = 'visited';
+            }, i * ANIMATION_SPEED);
+        }
+    }
+
     function adjustGridDimensions() {
         let board = document.getElementById('board');
         let windowWidth = window.innerWidth;
@@ -32,18 +65,26 @@ document.addEventListener('DOMContentLoaded', function() {
         let boardHeight = parseInt(board.style.height, 10);
         let numOfCols = Math.floor(boardWidth / 20);
         let numOfRows = Math.floor(boardHeight / 20);
+
+        gridBoard.rows = numOfRows;
+        gridBoard.columns = numOfCols;
+
+        START_ROW = Math.floor(numOfRows / 2);
+        START_COL = Math.floor(numOfCols / 4);
+        FINISH_ROW = Math.floor(numOfRows / 2);
+        FINISH_COL = Math.floor((numOfCols / 4) * 3);
+
         console.log(`${boardWidth}, ${boardHeight}`);
-        for (let row = 1; row <= numOfRows; row++) {
-            console.log(`${row}`);
-            for (let col = 1; col <= numOfCols; col++) {
-                console.log(`${col}`);
+        for (let row = 0; row < numOfRows; row++) {
+            const newGridArr = [];
+            for (let col = 0; col < numOfCols; col++) {
                 let newNodeIndex = `${row}-${col}`, newNodeClass, newNode;
 
-                if (row === Math.floor(boardHeight / 2) && col === Math.floor(boardWidth / 4)) {
+                if (row === START_ROW && col === START_COL) {
                     newNodeClass = 'start';
                 }
 
-                else if (row === Math.floor(boardHeight / 2) && col === Math.floor((boardWidth / 4) * 3)) {
+                else if (row === FINISH_ROW && col === FINISH_COL) {
                     newNodeClass = 'finish';
                 }
 
@@ -51,22 +92,25 @@ document.addEventListener('DOMContentLoaded', function() {
                     newNodeClass = 'unvisited';
                 }
 
-                newNode = new Node(newNodeIndex, newNodeClass);
+                newNode = new Node(newNodeIndex, row, col, newNodeClass);
+                // gridBoard.nodesMatrix.push(newNode);
+                newGridArr.push(newNode);
 
                 let newNodeDiv = document.createElement('div');
-                newNodeDiv.id = `${newNodeIndex}`;
-                newNodeDiv.class = `${newNodeClass}`;
+                newNodeDiv.id = `node-${newNodeIndex}`;
+                newNodeDiv.className = `${newNodeClass}`;
                 // console.log(newNodeDiv);
                 // newNodeDiv.push(newNode);
-                newNodeDiv.style.gridRow = `${row}`;
-                newNodeDiv.style.gridColumn = `${col}`;
+                /* CSS Grid Layout starts indexing at 1 instead of 0 */
+                newNodeDiv.style.gridRow = `${row + 1}`;
+                newNodeDiv.style.gridColumn = `${col + 1}`;
                 // console.log(newNodeDiv);
                 board.appendChild(newNodeDiv);
                 
             }
+            gridBoard.nodesMatrix.push(newGridArr);
         }
-
-        // console.log(board);
+        //console.log(gridBoard);
     }
 
     createGrid();
--- src/node.js
+++ src/node.js
@@ -1,12 +1,16 @@
 'use strict';
 
 export default class Node {
-    constructor(id, currClass) {
+    constructor(id, row, col, currClass) {
         this.id = id;
         this.class = currClass;
+        this.row = row;
+        this.column = col;
         this.weight = 0;
-        this.isStart = null;
-        this.isFinish = null;
+        this.isStart = false;
+        this.isFinish = false;
+        this.isVisited = false;
         this.distance = Infinity;
+        this.prevNode = null;
     }
 }
