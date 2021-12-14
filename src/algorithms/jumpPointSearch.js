93510251f4debe1909aaab5ac5cac776c82a690c
AF737 <83659747+AF737@users.noreply.github.com>
2021-12-14 21:22:46 +0100

    Add files via upload

--- src/algorithms/jumpPointSearch.js
+++ src/algorithms/jumpPointSearch.js
@@ -1,7 +1,8 @@
 'use strict';
 
 /* https://web.archive.org/web/20140310022652/https://zerowidth.com/2013/05/05/jump-point-search-explained.html 
-    http://users.cecs.anu.edu.au/~dharabor/data/papers/harabor-grastien-aaai11.pdf */
+    http://users.cecs.anu.edu.au/~dharabor/data/papers/harabor-grastien-aaai11.pdf 
+    https://www.gamedev.net/tutorials/programming/artificial-intelligence/jump-point-search-fast-a-pathfinding-for-uniform-cost-grids-r4220/ */
 function jumpPointSearch(grid, startNode, finishNode) {
     const closedList = [];
     startNode.distanceFromStart = 0;
