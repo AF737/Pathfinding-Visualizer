002aaab833937a8b4b0d77fb6d5b308fd1a119cc
AF737 <83659747+AF737@users.noreply.github.com>
2022-01-03 18:25:48 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -38,6 +38,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var clearWallsButton = document.getElementById('clearWalls');
     var clearWeightsButton = document.getElementById('clearWeights');
     var resetBoardButton = document.getElementById('resetBoard');
+    var infoBoxText = document.getElementById('infoBoxText');
+    var skipInfoBoxButton = document.getElementById('skipInfoBoxButton');
+    var prevInfoBoxButton = document.getElementById('prevInfoBoxButton');
+    var nextInfoButton = document.getElementById('nextInfoBoxButton');
+    var infoBoxPage = 0;
 
     /* TODO:
        - Clean up code
@@ -80,6 +85,110 @@ document.addEventListener('DOMContentLoaded', function() {
         removeWeights();
         resetStartAndFinish();
     });
+
+    skipInfoBoxButton.addEventListener('click', function() {
+        document.getElementById('infoBox').style.display = 'none';
+    });
+
+    prevInfoBoxButton.addEventListener('click', function() {
+        if (infoBoxPage > 0) {
+            infoBoxPage--;
+        }
+
+        displayInfoBoxText(infoBoxPage);
+    });
+
+    nextInfoButton.addEventListener('click', function() {
+        if (infoBoxPage < 5) {
+            infoBoxPage++;
+        }
+
+        displayInfoBoxText(infoBoxPage);
+    });
+
+    function displayInfoBoxText(currentPage) {
+        switch (currentPage) {
+            case 0:
+                infoBoxText.innerHTML = `<h2 class="h2InfoBox">Welcome to my Pathfinding
+                Visualizer</h2>
+                <h3 class="h3InfoBox">This project was inspired by Clément Mihailescu
+                    <br/>You can check out his pathfinding visualizer 
+                    <a href="https://github.com/clementmihailescu/Pathfinding-Visualizer">
+                    here</a>
+                </h3>
+                <p class="pInfoBox">This project was created to visualize different 
+                    pathfinding algorithms, but what is a that? These kinds of algorithms 
+                    are used to find the shortest path between to points (here nodes 
+                    on a 2d-grid). <br/>Please note that not all algorithms that are
+                    listed here <span style="font-weight: bold">guarantee</span> the shortest path. 
+                    <br/>You can skip this introductory tutorial at any point by clicking 
+                    the "Skip"-button. Otherwise press "Next" for more info about this project.
+                </p>`
+
+                break;
+            case 1:
+                infoBoxText.innerHTML = `<h2 class="h2InfoBox">Selecting an algorithm</h2>
+                <h3 class="h3InfoBox">You can select an algorithm from the first dropdown menu
+                    titled "Pick An Algorithm"
+                </h3>
+                <p class="pInfoBox">Please note that some of these algorithms are weighted 
+                    while others are unweighed. <br/> <span style="font-weight: bold">Weighted</span> 
+                    algorithms allow for weights to be placed on the 2d-grid, which are harder 
+                    to traverse than an empty field, but not impossible like walls. <br/> 
+                    <span style="font-weight: bold">Unweighted</span> algorithms do not allow for the 
+                    placement of weights.</p>`;
+
+                break;
+            case 2:
+                infoBoxText.innerHTML = `<h2 class="h2InfoBox">Info about each algorithm
+                1/2</h2>
+                <p class="pInfoBox">Dijkstra's algorithm (weighted): Explores all directions
+                    equally and guarantees the shortest path from start to finish.
+                    <br/>A* algorithm (weighted): Uses heuristics and the distance from start 
+                    to each node to first explore nodes which promise a short path therefore 
+                    reducing the number of nodes that need to be visited.
+                    Also guarantees the shortest path. <br/>Greedy best-first search (weighted): 
+                    Like A*, but it relies solely on heuristics, because it does not take the
+                    distance from the start to the current node into account. It is faster than
+                    A*, but does not guarantee the shortest path. <br/>Breadth-first search
+                    (unweighted): Like Dijkstra, but it does not take weights into account.
+                </p>`
+                
+                break;
+            case 3:
+                infoBoxText.innerHTML = `<h2 class="h2InfoBox">Info about each algorithm
+                2/2</h2>
+                <p class="pInfoBox">Bidirectional Dijkstra's algorithm (weighted): Here Dijkstra's algorithm
+                    begins from both the start and finsih node. The first point where both
+                    algorithms meet has to be part of the connecting path therefore not
+                    guaranteeing the shorest path. <br/>Bidirectional A* algorithm (weighted):
+                    Again both start and finish node use A* algorithm to reach each other and
+                    there is no guarantee that the found path is the shortest one. <br/>
+                    Depth-first search (unweighted): It explores every chosen path until it 
+                    reaches the edge of the grid without any heuristics and is therefore slow
+                    and and does not guarantee the shortest path. <br/>Jump Point Search 
+                    (unweighted): An improved version of the A* algorithm where instead of 
+                    checking each individual neighbor the algorithm jumps into these directions
+                    to see if there are any "jump points" (i.e. points next to walls) and then 
+                    starts jumping from there. This algorithm also guarantees the shortest path.
+                </p>`
+
+                break;
+            case 4:
+                infoBoxText.innerHTML = `<h2 class="h2InfoBox">Overview of some features</h2>
+                <p class="pInfoBox">Walls can be easily added by left-clicking on a tile and
+                    removed by once again left-clicking on that tile. <br/>There are three
+                    different weights which can be placed by left-clicking while pressing either of
+                    these buttons: <span style="color: #32ccb2">q</span>, <span style="color: #e8dd19">
+                    w</span>, <span style="color: #06d314">e</span>. Their values can be adjusted 
+                    individually by using the "Adjust Weights" menu. <br/>The start and finish node 
+                    can be moved by simply left-clicking on them and then clicking the tile where you 
+                    want to place them. <br/> <br/>
+                </p>
+                <img src="images/walls_weights_movement.gif"/>`;    
+                break;
+        }
+    }
 
     animateAlgorithmButton.addEventListener('click', function() {
         let selectedAlgorihtm = document.querySelector('input[name="algorithmOption"]:checked');
@@ -260,7 +369,7 @@ document.addEventListener('DOMContentLoaded', function() {
             boardWidth) / 2}px`;
         /* board.style.marginTop = `${(windowHeight - 
             boardHeight) / 2}px`; */
-        board.style.marginTop = `50px`;
+        board.style.marginTop = '50px';
         board.style.gridTemplateColumns = `${Math.floor(boardWidth / 20)}`;
         board.style.gridTemplateRows = `${Math.floor(boardHeight / 20)}`;
     }
